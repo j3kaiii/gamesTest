b042a0e32c847d9e1c4e087d96926e83eb4cce89
micro <j3kaiii@gmail.com>
2023-01-25 13:57:40 +0300

    кастом цвет?

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'titleTextColor': '#de6c21'
+      },
+    },
   },
   plugins: [],
 }
